ab4351d987f63b3cec77d6479f83c6451be26a7d
Hasitha Dulanjana <hasithapalihena100@gmail.com>
2018-03-21 12:21:31 +1300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Secuirty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void uplaodImage(object sender, RoutedEventArgs e)
+        {
+
+         
+        }
+    }
+}
